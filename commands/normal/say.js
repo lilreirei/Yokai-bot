e4f0d76c95be1e17f71b4b7fd631cf9f38627827
HaremK1ng <pepijn.vanden.broek@gmail.com>
2017-03-26 19:17:37 +0200

    Echo commands use embeds now

--- commands/normal/say.js
+++ commands/normal/say.js
@@ -1,8 +1,34 @@
+const COLORSDECIAML = [
+    0x2B54CE,
+    0xFF8000,
+    0x9932CC,
+    0x008080,
+    0x800080,
+    0x808080,
+    0xEE82EE,
+    0xFFB6C1,
+    0x86B3E8,
+    0x93FFAA,
+    0x979E79
+];
+
 module.exports = {
 	desc: "Echo",
 	usage: "<text>",
 	aliases: ['echo'],
 	task(bot, msg, suffix) {
-		bot.createMessage(msg.channel.id, `:speech_balloon: ${suffix}` || 'echo');
+		let choose = ~~(Math.random() * COLORSDECIAML.length);
+		var color = COLORSDECIAML[choose];
+		bot.createMessage(msg.channel.id, { content: ``,
+			embed: {
+				color: color,
+				author: {
+					name: ``,
+					url: ``,
+					icon_url: ``
+				},
+				description: `:speech_balloon: ${suffix}` || 'echo'
+			}
+		});
 	}
 };
--- commands/normal/sayd.js
+++ commands/normal/sayd.js
@@ -1,9 +1,35 @@
+const COLORSDECIAML = [
+    0x2B54CE,
+    0xFF8000,
+    0x9932CC,
+    0x008080,
+    0x800080,
+    0x808080,
+    0xEE82EE,
+    0xFFB6C1,
+    0x86B3E8,
+    0x93FFAA,
+    0x979E79
+];
+
 module.exports = {
 	desc: "Echo and deletes the command message.",
 	usage: "<text>",
 	aliases: ['echod'],
 	task(bot, msg, suffix) {
-		bot.createMessage(msg.channel.id, `:speech_balloon: ${suffix}` || 'echo').then(sentMsg => {
+		let choose = ~~(Math.random() * COLORSDECIAML.length);
+		var color = COLORSDECIAML[choose];
+		bot.createMessage(msg.channel.id, { content: ``,
+			embed: {
+				color: color,
+				author: {
+					name: ``,
+					url: ``,
+					icon_url: ``
+				},
+				description: `:speech_balloon: ${suffix}` || 'echo'
+			}
+		}).then(sentMsg => {
 			bot.deleteMessage(sentMsg.channel.id, msg.id);
 		})
 	}
