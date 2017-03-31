e030ba53cb6f6ce7cb7dba84b37bbb8664af1f02
HaremK1ng <pepijn.vanden.broek@gmail.com>
2017-03-31 03:16:18 +0200

    Fix changelog command

--- commands/normal/changelog.js
+++ commands/normal/changelog.js
@@ -1,21 +1,39 @@
+const COLORSDECIAML = [
+    0x2B54CE,
+    0xFF8000,
+    0x9932CC,
+    0x008080,
+    0x800080,
+    0x808080,
+    0xEE82EE,
+    0xFFB6C1,
+    0x86B3E8,
+    0x93FFAA,
+    0x979E79
+];
+
 module.exports = {
 	desc: "Sends the latest changelog from the support server.",
 	usage: "",
 	guildOnly: true,
 	task(bot, msg, suffix, channel) {
-		let c_ = bot.getChannel('227728707555426315')
+		let choose = ~~(Math.random() * COLORSDECIAML.length);
+		var color = COLORSDECIAML[choose];
+		let c_ = bot.getChannel('240154536856125440')
 
 		c_.getMessages(0).then(function(value) {
 			let embed = {
-			color: 12391760,
+			color: color,
 	           author: {
 	            name: 'Changelog',
-	             icon_url: 'https://cdn.discordapp.com/avatars/237578660708745216/95ce0b98a1ef4774d4e9c586b5a45121.jpg'
+	             icon_url: 'https://b.catgirlsare.sexy/kMDk.jpg'
 	           },
-	           fields: [{
-							name: `Latest changelog:`,
-	             value: `${value[0].content}`
-						}]
+	           fields: [
+							 {
+								 name: `Latest changelog:`,
+								 value: `${value[0].content}`
+							 }
+					]
 	       }
 			bot.createMessage(msg.channel.id,{embed: embed});
 		})
