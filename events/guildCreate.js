1c8d3e4663ed713f4086e8d29bd89deaa727e7b0
HaremK1ng <pepijn.vanden.broek@gmail.com>
2017-04-08 17:26:32 +0200

    Fixed small typo

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -12,5 +12,5 @@ module.exports = function(bot, _settingsManager, config, guild) {
 	} else if (config.nowelcomemessageGuild.includes(guild.id))
 			logger.logWithHeader('DIDNT SEND WELCOME MESSGAE', 'bgBlue', 'black', guild.name);
 	else
-		guild.defaultChannel.createMessage("Awesome a new server!\nA few commands to help you out: `s!help` for normal commands and `s$help` for moderation commands.\nYou could also view all my commands on https://commands.shinobubot.xyz (this part of the site is sill in development by my master) :heart:");
+		guild.defaultChannel.createMessage("Awesome a new server!\nA few commands to help you out: `s!help` for normal commands and `s$help` for moderation commands.\nYou could also view all my commands on https://commands.shinobubot.xyz (this part of the site is still in development by my master) :heart:");
 }
