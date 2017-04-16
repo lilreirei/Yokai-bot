6a4f2738dc3fd4e4aae64b13221d27574022fb68
HaremK1ng <pepijn.vanden.broek@gmail.com>
2017-04-16 16:32:04 +0200

    Hopefully fixed gif command

--- commands/all/gif.js
+++ commands/all/gif.js
@@ -1,45 +1,45 @@
-const fs = require('fs');
-const request = require('request');
+var giphy = require('giphy-api')();
 
 module.exports = {
-	desc: "Sends a gif from giphy using your search terms.",
-	usage: "<tags>",
-	task(bot, msg, suffix) {
-		var url = "http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=" + suffix;
-        download( url, 'gif.json', function(){
-            var json = JSON.parse(fs.readFileSync('gif.json', 'utf8'));
-						bot.createMessage(msg.channel.id, `${json.data.url || 'gif'}`).catch(err => {
-		          bot.createMessage(msg.channel.id, { content: ``,
-		            embed: {
-		              color: 0xf4ce11,
-		              author: {
-		                name: ``,
-		                url: ``,
-		                icon_url: ``
-		              },
-		              description: `${err}`
-		            }
-		          })
-		        });
-        });
-	}
-};
-
-var download = function(uri, filename, callback){
-  request.head(uri, function(err, res, body){
-		if(err) {
-			bot.createMessage(msg.channel.id, { content: ``,
-				embed: {
-					color: 0xf4ce11,
-					author: {
-						name: ``,
-						url: ``,
-						icon_url: ``
-					},
-					description: `${err}`
-				},
-			});
-		}
-    request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
-  });
+  desc: "Sends a gif from giphy using your search terms.",
+  usage: "<tags> | nothing for absolutely random",
+  task(bot, msg, args) {
+    giphy.random(`${args}`).then(function(res) {
+      bot.createMessage(msg.channel.id, {
+        content: ``,
+        embed: {
+          color: 0xf4ce11,
+          author: {
+            name: ``,
+            url: ``,
+            icon_url: ``
+          },
+          description: `[Direct image url](${res.data.image_url})
+Frames: ${res.data.image_frames}
+Width: ${res.data.image_width}
+Height: ${res.data.image_height}`,
+          image: {
+            url: `${res.data.image_url}`
+          },
+          footer: {
+            text: `${msg.channel.guild ? (`${msg.channel.guild.name} : #${msg.channel.name}`) : ""}`,
+            icon_url: `${msg.channel.guild.iconURL === null ? `` : ''}${msg.channel.guild.iconURL !== null ? msg.channel.guild.iconURL : ''}`
+          }
+        }
+      })
+    }).catch(err => {
+      bot.createMessage(msg.channel.id, {
+        content: ``,
+        embed: {
+          color: 0xf4ce11,
+          author: {
+            name: ``,
+            url: ``,
+            icon_url: ``
+          },
+          description: `${err}`
+        }
+      })
+    });
+  }
 };
