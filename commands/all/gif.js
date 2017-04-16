1920a8b82c81f15492793d4edd0028a906aaac63
HaremK1ng <pepijn.vanden.broek@gmail.com>
2017-04-16 05:50:17 +0200

    Hopefully fixed error.

--- commands/all/gif.js
+++ commands/all/gif.js
@@ -8,14 +8,38 @@ module.exports = {
 		var url = "http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=" + suffix;
         download( url, 'gif.json', function(){
             var json = JSON.parse(fs.readFileSync('gif.json', 'utf8'));
-            bot.createMessage(msg.channel.id, json.data.url || 'gif');
+						bot.createMessage(msg.channel.id, `${json.data.url || 'gif'}`).catch(err => {
+		          bot.createMessage(msg.channel.id, { content: ``,
+		            embed: {
+		              color: 0xf4ce11,
+		              author: {
+		                name: ``,
+		                url: ``,
+		                icon_url: ``
+		              },
+		              description: `${err}`
+		            }
+		          })
+		        });
         });
 	}
 };
 
 var download = function(uri, filename, callback){
   request.head(uri, function(err, res, body){
-
+		if(err) {
+			bot.createMessage(msg.channel.id, { content: ``,
+				embed: {
+					color: 0xf4ce11,
+					author: {
+						name: ``,
+						url: ``,
+						icon_url: ``
+					},
+					description: `${err}`
+				},
+			});
+		}
     request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
   });
 };
