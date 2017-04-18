6751868a9218cdf4cc7c48244f0dc7d0287dff05
HaremK1ng <pepijn.vanden.broek@gmail.com>
2017-04-18 21:53:34 +0200

    Change booru, add sfwbooru

--- commands/all/booru.js
+++ commands/all/booru.js
@@ -1,34 +1,34 @@
 const booru = require('booru')
 
 module.exports = {
-    desc: "Send possibly nsfw image with the given tag(s) (Max 2 tags, seperate tags by space!\n(Use either s=safe, q=questionable or e=explicit for a tag to choose what you want) [nsfw]",
-    usage: "<site> [<tag1> <tag2>] (Max 2 tags, tags must be seperated by space!).\nType: \"s.booru list\" for a list of sites the bot can get a picture from.",
-    aliases: ['nsfw', 'hentai'],
-    cooldown: 5,
-    guildOnly: true,
-    task(bot, msg, args, config, settingsManager) {
-        var nsfw = settingsManager.getNSFW(msg.channel.guild.id, msg.channel.id);
-        if (!nsfw) {
-            bot.createMessage(msg.channel.id, 'You can only use this command in an **nsfw** channels, use \`s.settings nsfw <allow/deny>\`.');
-        } else if (!args) {
-            return 'wrong usage'
-        } else {
-            var str = args + "";
-            var array = str.split(' '),
-                a = array[0],
-                b = array[1],
-                c = array[2];
+  desc: "Send possibly nsfw image with the given tag(s) (Max 2 tags, seperate tags by space!\n(Use either s=safe, q=questionable or e=explicit for a tag to choose what you want) [nsfw]",
+  usage: "<site> [<tag1> <tag2>] (Max 2 tags, tags must be seperated by space!).\nType: \"s.booru list\" for a list of sites the bot can get a picture from.",
+  aliases: ['nsfw', 'hentai'],
+  cooldown: 5,
+  guildOnly: true,
+  task(bot, msg, args, config, settingsManager) {
+    var nsfw = settingsManager.getNSFW(msg.channel.guild.id, msg.channel.id);
+    if (!nsfw) {
+      bot.createMessage(msg.channel.id, 'You can only use this command in an **nsfw** channels, use \`s.settings nsfw <allow/deny>\`.');
+    } else if (!args) {
+      return 'wrong usage'
+    } else {
+      var str = args + "";
+      var array = str.split(' '),
+        a = array[0],
+        b = array[1],
+        c = array[2];
 
-            if (a === 'list') {
-                bot.createMessage(msg.channel.id, {
-                    content: ``,
-                    embed: {
-                        color: 0xf4ce11,
-                        author: {
-                            name: `${msg.author.username}`,
-                            icon_url: `${msg.author.avatarURL}`
-                        },
-                        description: `e621.net, aliases: ["e6","e621"]
+      if (a === 'list') {
+        bot.createMessage(msg.channel.id, {
+          content: ``,
+          embed: {
+            color: 0xf4ce11,
+            author: {
+              name: `${msg.author.username}`,
+              icon_url: `${msg.author.avatarURL}`
+            },
+            description: `e621.net, aliases: ["e6","e621"]
 e926.net, aliases: ["e9","e926"]
 hypnohub.net, aliases: ["hh","hypo","hypohub"]
 danbooru.donmai.us, aliases: ["db","dan","danbooru"]
@@ -44,131 +44,177 @@ youhate.us, aliases: ["yh","you","youhate"]
 dollbooru.org, aliases: ["do","doll","dollbooru"]
 rule34.paheal.net, aliases: ["pa","paheal"]
 lolibooru.moe, aliases: ["lb", "lol", "loli", "lolibooru"]`,
-                        footer: {
-                            text: `${msg.channel.guild ? (`${msg.channel.guild.name} : #${msg.channel.name}`) : ""}`,
-                            icon_url: `${msg.channel.guild.iconURL === null ? `` : ''}${msg.channel.guild.iconURL !== null ? msg.channel.guild.iconURL : ''}`
-                        }
-                    }
-                })
-            } else {
-                if (!c) {
-                    booru.search(`${a}`, [`${b}`], {
-                            limit: 1,
-                            random: true
-                        })
-                        .then(booru.commonfy)
-                        .then(images => {
-                            for (let image of images) {
-                              var tag = image.common.tags + "";
-                              var tags = tag.split(',').join(', ');
-                              var img = image.common.file_url.toString(" ");
-                              var imguri = img.replace(/ /g, "%20");
-                                bot.createMessage(msg.channel.id, {
-                                    content: ``,
-                                    embed: {
-                                        color: 0xf4ce11,
-                                        author: {
-                                            name: `${msg.author.username}`,
-                                            url: `${imguri}`,
-                                            icon_url: `${msg.author.avatarURL}`
-                                        },
-                                        description: `[Click here for the direct image url](${imguri})
+            footer: {
+              text: `${msg.channel.guild ? (`${msg.channel.guild.name} : #${msg.channel.name}`) : ""}`,
+              icon_url: `${msg.channel.guild.iconURL === null ? `` : ''}${msg.channel.guild.iconURL !== null ? msg.channel.guild.iconURL : ''}`
+            }
+          }
+        })
+      } else {
+        if (!c) {
+          booru.search(`${a}`, [`${b}`], {
+              limit: 1,
+              random: true
+            })
+            .then(booru.commonfy)
+            .then(images => {
+              for (let image of images) {
+                var tag = image.common.tags + "";
+                var tags = tag.split(',').join(', ');
+                var img = image.common.file_url.toString(" ");
+                var imguri = img.replace(/ /g, "%20");
+                bot.createMessage(msg.channel.id, {
+                  content: ``,
+                  embed: {
+                    color: 0xf4ce11,
+                    author: {
+                      name: `${msg.author.username}`,
+                      url: `${imguri}`,
+                      icon_url: `${msg.author.avatarURL}`
+                    },
+                    description: `[Click here for the direct image url](${imguri})
 Searched tags: ${b}
 Tags: ${tags}
 Score: ${image.common.score}
 Rating: ${image.common.rating}`,
-                                        image: {
-                                            url: `${imguri}`
-                                        },
-                                        footer: {
-                                            text: `${msg.channel.guild ? (`${msg.channel.guild.name} : #${msg.channel.name}`) : ""}`,
-                                            icon_url: `${msg.channel.guild.iconURL === null ? `` : ''}${msg.channel.guild.iconURL !== null ? msg.channel.guild.iconURL : ''}`
-                                        }
-                                    }
-                                }).catch(err => {
-                                  bot.createMessage(msg.channel.id, { content: ``,
-                                    embed: {
-                                      color: 0xf4ce11,
-                                      author: {
-                                        name: ``,
-                                        url: ``,
-                                        icon_url: ``
-                                      },
-                                      description: `${err}`
-                                    }
-                                  })
-                                });
-                            }
-                        })
-                        .catch(err => {
-                            if (err.name === 'booruError') {
-                                console.log(err.message)
-                                bot.createMessage(msg.channel.id, `No image found with this tag please try again.`)
-                            } else {
-                                console.log(err)
-                                bot.createMessage(msg.channel.id, `${err}`)
-                            }
-                        })
-                } else {
-                    booru.search(`${a}`, [`${b}`, `${c}`], {
-                            limit: 1,
-                            random: true
-                        })
-                        .then(booru.commonfy)
-                        .then(images => {
-                            for (let image of images) {
-                              var tag = image.common.tags + "";
-                              var tags = tag.split(',').join(', ');
-                              var img = image.common.file_url.toString(" ");
-                              var imguri = img.replace(/ /g, "%20");
-                                bot.createMessage(msg.channel.id, {
-                                    content: ``,
-                                    embed: {
-                                        color: 0xf4ce11,
-                                        author: {
-                                            name: `${msg.author.username}`,
-                                            url: `${imguri}`,
-                                            icon_url: `${msg.author.avatarURL}`
-                                        },
-                                        description: `[Click here for the direct image url](${imguri})
+                    image: {
+                      url: `${imguri}`
+                    },
+                    footer: {
+                      text: `${msg.channel.guild ? (`${msg.channel.guild.name} : #${msg.channel.name}`) : ""}`,
+                      icon_url: `${msg.channel.guild.iconURL === null ? `` : ''}${msg.channel.guild.iconURL !== null ? msg.channel.guild.iconURL : ''}`
+                    }
+                  }
+                }).catch(err => {
+                  bot.createMessage(msg.channel.id, {
+                    content: ``,
+                    embed: {
+                      color: 0xf4ce11,
+                      author: {
+                        name: ``,
+                        url: ``,
+                        icon_url: ``
+                      },
+                      description: `${err}`
+                    }
+                  })
+                });
+              }
+            })
+            .catch(err => {
+              if (err.name === 'booruError') {
+                console.log(err.message)
+                bot.createMessage(msg.channel.id, {
+                  content: ``,
+                  embed: {
+                    color: 0xf4ce11,
+                    author: {
+                      name: ``,
+                      url: ``,
+                      icon_url: ``
+                    },
+                    description: `${err.message}`
+                  }
+                })
+              } else {
+                console.log(err)
+                bot.createMessage(msg.channel.id, {
+                  content: ``,
+                  embed: {
+                    color: 0xf4ce11,
+                    author: {
+                      name: ``,
+                      url: ``,
+                      icon_url: ``
+                    },
+                    description: `${err}`
+                  }
+                })
+              }
+            })
+        } else {
+          booru.search(`${a}`, [`${b}`, `${c}`], {
+              limit: 1,
+              random: true
+            })
+            .then(booru.commonfy)
+            .then(images => {
+              for (let image of images) {
+                var tag = image.common.tags + "";
+                var tags = tag.split(',').join(', ');
+                var img = image.common.file_url.toString(" ");
+                var imguri = img.replace(/ /g, "%20");
+                bot.createMessage(msg.channel.id, {
+                  content: ``,
+                  embed: {
+                    color: 0xf4ce11,
+                    author: {
+                      name: `${msg.author.username}`,
+                      url: `${imguri}`,
+                      icon_url: `${msg.author.avatarURL}`
+                    },
+                    description: `[Click here for the direct image url](${imguri})
 Searched tags: ${b}, ${c}
 Tags: ${tags}
 Score: ${image.common.score}
 Rating: ${image.common.rating}`,
-                                        image: {
-                                            url: `${imguri}`
-                                        },
-                                        footer: {
-                                            text: `${msg.channel.guild ? (`${msg.channel.guild.name} : #${msg.channel.name}`) : ""}`,
-                                            icon_url: `${msg.channel.guild.iconURL === null ? `` : ''}${msg.channel.guild.iconURL !== null ? msg.channel.guild.iconURL : ''}`
-                                        }
-                                    }
-                                }).catch(err => {
-                                  bot.createMessage(msg.channel.id, { content: ``,
-                                    embed: {
-                                      color: 0xf4ce11,
-                                      author: {
-                                        name: ``,
-                                        url: ``,
-                                        icon_url: ``
-                                      },
-                                      description: `${err}`
-                                    }
-                                  })
-                                });
-                            }
-                        })
-                        .catch(err => {
-                            if (err.name === 'booruError') {
-                                console.log(err.message)
-                                bot.createMessage(msg.channel.id, `No image found with these tags please try again.`)
-                            } else {
-                                console.log(err)
-                                bot.createMessage(msg.channel.id, `${err}`)
-                            }
-                        })
-                }
-            }
+                    image: {
+                      url: `${imguri}`
+                    },
+                    footer: {
+                      text: `${msg.channel.guild ? (`${msg.channel.guild.name} : #${msg.channel.name}`) : ""}`,
+                      icon_url: `${msg.channel.guild.iconURL === null ? `` : ''}${msg.channel.guild.iconURL !== null ? msg.channel.guild.iconURL : ''}`
+                    }
+                  }
+                }).catch(err => {
+                  bot.createMessage(msg.channel.id, {
+                    content: ``,
+                    embed: {
+                      color: 0xf4ce11,
+                      author: {
+                        name: ``,
+                        url: ``,
+                        icon_url: ``
+                      },
+                      description: `${err}`
+                    }
+                  })
+                });
+              }
+            })
+            .catch(err => {
+              if (err.name === 'booruError') {
+                console.log(err.message)
+                bot.createMessage(msg.channel.id, {
+                  content: ``,
+                  embed: {
+                    color: 0xf4ce11,
+                    author: {
+                      name: ``,
+                      url: ``,
+                      icon_url: ``
+                    },
+                    description: `${err.message}`
+                  }
+                })
+              } else {
+                console.log(err)
+                bot.createMessage(msg.channel.id, {
+                  content: ``,
+                  embed: {
+                    color: 0xf4ce11,
+                    author: {
+                      name: ``,
+                      url: ``,
+                      icon_url: ``
+                    },
+                    description: `${err}`
+                  }
+                })
+              }
+            })
         }
+      }
     }
+  }
 };
---
+++ commands/all/sfwbooru.js
@@ -0,0 +1,116 @@
+const booru = require('sfwbooru');
+
+module.exports = {
+  desc: "Sends random image from a sfw booru site.",
+  usage: "<site>\nType: \"s.sfwbooru list\" for a list of sites the bot can get a picture from.",
+  aliases: ["sfw"],
+  cooldown: 5,
+  guildOnly: true,
+  task(bot, msg, args) {
+    if (!args)
+      return 'wrong usage';
+    const site = args;
+    const lower = site.toLowerCase();
+    if (lower === 'list') {
+      bot.createMessage(msg.channel.id, {
+        content: ``,
+        embed: {
+          color: 0xf4ce11,
+          author: {
+            name: `${msg.author.username}`,
+            icon_url: `${msg.author.avatarURL}`
+          },
+          description: `e926.net, aliases: ["e9","e926"]
+konachan.net, aliases: ["kn","konan","knet"]
+safebooru.org, aliases: ["sb","safe","safebooru"]
+tbib.org, aliases: ["tb", "tbib","big"]
+dollbooru.org, aliases: ["do","doll","dollbooru"]`,
+          footer: {
+            text: `${msg.channel.guild ? (`${msg.channel.guild.name} : #${msg.channel.name}`) : ""}`,
+            icon_url: `${msg.channel.guild.iconURL === null ? `` : ''}${msg.channel.guild.iconURL !== null ? msg.channel.guild.iconURL : ''}`
+          }
+        }
+      })
+    } else {
+      booru.search(site, ['', '', 's'], {
+          limit: 1,
+          random: true
+        })
+        .then(booru.commonfy)
+        .then(images => {
+          for (let image of images) {
+            const tag = image.common.tags + "";
+            const tags = tag.split(',').join(', ');
+            const img = image.common.file_url.toString(" ");
+            const imguri = img.replace(/ /g, "%20");
+            bot.createMessage(msg.channel.id, {
+              content: ``,
+              embed: {
+                color: 0xf4ce11,
+                author: {
+                  name: `${msg.author.username}`,
+                  url: `${imguri}`,
+                  icon_url: `${msg.author.avatarURL}`
+                },
+                description: `[Click here for the direct image url](${imguri})
+Tags: ${tags}
+Score: ${image.common.score}
+Rating: ${image.common.rating}`,
+                image: {
+                  url: `${imguri}`
+                },
+                footer: {
+                  text: `${msg.channel.guild ? (`${msg.channel.guild.name} : #${msg.channel.name}`) : ""}`,
+                  icon_url: `${msg.channel.guild.iconURL === null ? `` : ''}${msg.channel.guild.iconURL !== null ? msg.channel.guild.iconURL : ''}`
+                }
+              }
+            }).catch(err => {
+              bot.createMessage(msg.channel.id, {
+                content: ``,
+                embed: {
+                  color: 0xf4ce11,
+                  author: {
+                    name: ``,
+                    url: ``,
+                    icon_url: ``
+                  },
+                  description: `${err}`
+                }
+              })
+            });
+          }
+        })
+        .catch(err => {
+          if (err.name === 'booruError') {
+            console.log(err.message)
+            bot.createMessage(msg.channel.id, {
+              content: ``,
+              embed: {
+                color: 0xf4ce11,
+                author: {
+                  name: ``,
+                  url: ``,
+                  icon_url: ``
+                },
+                description: `${err.message}`
+              }
+            })
+          } else {
+            console.log(err)
+            bot.createMessage(msg.channel.id, {
+              content: ``,
+              embed: {
+                color: 0xf4ce11,
+                author: {
+                  name: ``,
+                  url: ``,
+                  icon_url: ``
+                },
+                description: `${err}`
+              }
+            })
+          }
+        })
+    }
+  }
+};
