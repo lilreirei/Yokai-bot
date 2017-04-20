07f3e25381d4a5541f75e6578fdd50bbfb6e5922
HaremK1ng <pepijn.vanden.broek@gmail.com>
2017-04-20 02:09:44 +0200

    Fix prune

--- commands/all/prune.js
+++ commands/all/prune.js
@@ -1,21 +1,20 @@
 module.exports = {
-	desc: "Prunes the given number of messages. If no number is given it's standard 100.",
-	usage: "<number to prune>",
+  desc: "Prunes the given number of messages. If no number is given it's standard 100.",
+  usage: "<number to prune>",
   aliases: ['purge', 'clear'],
   guildOnly: true,
-	requiredPermission: 'manageMessages',
-	task(bot, msg, suffix) {
+  requiredPermission: 'manageMessages',
+  task(bot, msg, suffix) {
     var limit = '';
 
-    if(!suffix){
+    if (!suffix) {
       limit = 100 + 1;
-    }
-    else if(suffix){
-			var count = parseInt(suffix);
-			msgTodelete = count + 1;
-      limit = msgTodelete;
+    } else if (suffix) {
+      var count = parseInt(suffix),
+        msgTodelete = count + 1,
+        limit = msgTodelete;
     }
 
     bot.purgeChannel(msg.channel.id, limit)
-	}
+  }
 };
