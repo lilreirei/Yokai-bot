350b9a9beb7fe81ca449040b97d84595a8a2729a
HaremK1ng <pepijn.vanden.broek@gmail.com>
2017-04-26 16:30:13 +0200

    Add confirmation message to help command

--- utils/CommandManager.js
+++ utils/CommandManager.js
@@ -141,6 +141,7 @@ class CommandManager {
 				currentMessage += '\n' + toAdd;
 			}
 			messageQueue.push(currentMessage);
+			bot.createMessage(msg.channel.id, ':white_check_mark: Help has been send to your DMs.');
 			bot.getDMChannel(msg.author.id).then(chan => {
 				let sendInOrder = setInterval(() => { //eslint-disable-line no-unused-vars
 					if (messageQueue.length > 0)
