d43689cc2e600f968afc96fda7e8dbabd8363ce9
HaremK1ng <pepijn.vanden.broek@gmail.com>
2017-05-01 16:08:26 +0200

    Change lmgt to lmgtfy

---
+++ commands/all/lmgtfy.js
@@ -0,0 +1,35 @@
+const lmgtfy = require('lmgtfy');
+
+module.exports = {
+  desc: "Let me google that.",
+  usage: "<text>",
+  cooldown: 5,
+  task(bot, msg, args) {
+		if (!args) return 'wrong usage';
+    let result = lmgtfy(`${args}`);
+		if (!result) return bot.createMessage(msg.channel.id, {
+			content: ``,
+			embed: {
+				color: 0xf4ce11,
+				author: {
+					name: ``,
+					url: ``,
+					icon_url: ``
+				},
+				description: `Ewps couldn't get any results.`
+			}
+		});
+		bot.createMessage(msg.channel.id, {
+			content: ``,
+			embed: {
+				color: 0xf4ce11,
+				author: {
+					name: ``,
+					url: ``,
+					icon_url: ``
+				},
+				description: `:arrow_right: ${result}`
+			}
+		});
+  }
+};
