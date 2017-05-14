0c5fa62529257d0621bfb1858756a6d8f31acb53
HaremK1ng <pepijn.vanden.broek@gmail.com>
2017-05-14 16:10:43 +0200

    Add ID check for hackban

--- commands/all/hackban.js
+++ commands/all/hackban.js
@@ -10,22 +10,44 @@ module.exports = {
             userToBan = array[0],
             reason = array[1];
         const deletedays = 7;
-        msg.channel.guild.members.get(bot.user.id).permission.json.banMembers
-        bot.banGuildMember(msg.channel.guild.id, userToBan, deletedays, reason).catch(err => {
-            var string = `${err}`,
-                substring = 'Privilege is too low...';
-            if (string.includes(substring)) return bot.createMessage(msg.channel.id, {
-                content: ``,
-                embed: {
-                    color: 0xff0000,
-                    author: {
-                        name: ``,
-                        url: ``,
-                        icon_url: ``
-                    },
-                    description: `Can't ban <@${userToBan}>, privilege is too low.`
-                }
-            })
+        const strlength = /^\d{17,18}/.test(userToBan);
+        const isnum = /^\d+$/.test(userToBan);
+        if (isnum === true && strlength === true) {
+            msg.channel.guild.members.get(bot.user.id).permission.json.banMembers
+            bot.banGuildMember(msg.channel.guild.id, userToBan, deletedays, reason).catch(err => {
+                var string = `${err}`,
+                    substring = 'Privilege is too low...';
+                if (string.includes(substring)) return bot.createMessage(msg.channel.id, {
+                    content: ``,
+                    embed: {
+                        color: 0xff0000,
+                        author: {
+                            name: ``,
+                            url: ``,
+                            icon_url: ``
+                        },
+                        description: `Can't ban <@${userToBan}>, privilege is too low.`
+                    }
+                })
+                bot.createMessage(msg.channel.id, {
+                    content: ``,
+                    embed: {
+                        color: 0xff0000,
+                        author: {
+                            name: ``,
+                            url: ``,
+                            icon_url: ``
+                        },
+                        description: `${err}`,
+                        fields: [{
+                            name: `For support join:`,
+                            value: `https://discord.gg/Vf4ne5b`,
+                            inline: true
+                        }]
+                    }
+                });
+            });
+        } else {
             bot.createMessage(msg.channel.id, {
                 content: ``,
                 embed: {
@@ -35,7 +57,7 @@ module.exports = {
                         url: ``,
                         icon_url: ``
                     },
-                    description: `${err}`,
+                    description: `The given ID is invalid, make sure you used a correct userID.`,
                     fields: [{
                         name: `For support join:`,
                         value: `https://discord.gg/Vf4ne5b`,
@@ -43,6 +65,6 @@ module.exports = {
                     }]
                 }
             });
-        });
+        }
     }
 };
